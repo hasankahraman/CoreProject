3c631eff4260d16bb0dc5cd31c94e360f56e204f
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-02-18 21:08:02 +0300

    6262

---
+++ CoreProject/ViewComponents/Blog/VCBlogListDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreProject.ViewComponents.Blog
+{
+	public class VCBlogListDashboard : ViewComponent
+	{
+		BlogManager manager = new BlogManager(new EFBlogRepository());
+
+		public IViewComponentResult Invoke()
+		{
+			var values = manager.GetAllWithCategory();
+			return View(values);
+		}
+	}
+}
