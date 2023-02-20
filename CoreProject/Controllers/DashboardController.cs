1022d4e63edbfeb1340a5e43fd9a07d038c1746b
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-02-20 16:30:35 +0300

    70 finished

--- CoreProject/Controllers/DashboardController.cs
+++ CoreProject/Controllers/DashboardController.cs
@@ -1,14 +1,23 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace CoreProject.Controllers
 {
 	public class DashboardController : Controller
 	{
+		BlogManager blogManager = new BlogManager(new EFBlogRepository());
+		CategoryManager categoryManager = new CategoryManager(new EFCategoryRepository());
+
 		[AllowAnonymous]
 		public IActionResult Index()
 		{
-			return View();
+			ViewBag.blogCount = blogManager.GetAll().Count;
+            ViewBag.writerBlogCount = blogManager.GetAll().Where(x => x.WriterId == 1).Count();
+            ViewBag.categoryCount = categoryManager.GetAll().Count;
+            return View();
 		}
 	}
 }
