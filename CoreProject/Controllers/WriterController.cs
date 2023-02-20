77624ddd14ae706713b903757eae73905e3401da
HASAN KAHRAMAN <hasankahraman45@outlook.com>
2023-02-21 00:42:01 +0300

    73 finished

--- CoreProject/Controllers/WriterController.cs
+++ CoreProject/Controllers/WriterController.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BussinessLayer.Concrete;
+using BussinessLayer.ValidationRules;
+using CoreProject.Models;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
 
 namespace CoreProject.Controllers
 {
 	public class WriterController : Controller
 	{
+		WriterManager manager = new WriterManager(new EFWriterRepository());
 		public IActionResult Index()
 		{
 			return View();
@@ -23,6 +32,66 @@ namespace CoreProject.Controllers
         public PartialViewResult PartialWriterFooter()
         {
             return PartialView();
+        }
+        [AllowAnonymous]
+		[HttpGet]
+        public IActionResult WriterProfileUpdate()
+        {
+			var values = manager.GetById(1);
+            return View(values);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterProfileUpdate(Writer writer)
+        {
+            WriterValidator validator = new WriterValidator();
+            ValidationResult result = validator.Validate(writer);
+            writer.Status = true;
+            writer.Image = "/coreblogtema/web/images/t1.jpg";
+
+            if (result.IsValid)
+            {
+                manager.Update(writer);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterAdd()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterAdd(AddProfileImage p)
+        {
+            Writer writer = new Writer();
+            if (p.Image != null)
+            {
+                var extension = Path.GetExtension(p.Image.FileName);
+                var newImageName = Guid.NewGuid() + extension;
+                var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/ImageFiles/", newImageName);
+                var stream = new FileStream(location, FileMode.Create);
+                p.Image.CopyTo(stream);
+                writer.Image = newImageName;
+            }
+            writer.Email = p.Email;
+            writer.Name = p.Name;
+            writer.Password = p.Password;
+            writer.Status = p.Status;
+            writer.About = p.About;
+
+            manager.Add(writer);
+            return RedirectToAction("Index","Dashboard");
         }
     }
 }
---
+++ CoreProject/Models/AddProfileImage.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace CoreProject.Models
+{
+    public class AddProfileImage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+        public IFormFile Image { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool Status { get; set; }
+    }
+}
